d3f83f1be9240a6629af2ddcd9de2f4803688923
kyle uyehara <kuyehara1@gmail.com>
2016-09-25 12:44:04 -1000

    complete exercise2 with the implementation of module pattern

--- JS_Adv/exercises/ex2/ex2.js
+++ JS_Adv/exercises/ex2/ex2.js
@@ -1,100 +1,130 @@
 // assume this data came from the database
-var notes = [
-	"This is the first note I've taken!",
-	"Now is the time for all good men to come to the aid of their country.",
-	"The quick brown fox jumped over the moon."
-];
+var NotesManager = (function notesManager() {
+  var notes = [
+  	"This is the first note I've taken!",
+  	"Now is the time for all good men to come to the aid of their country.",
+  	"The quick brown fox jumped over the moon.",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1",
+    "bulk1"
+  ];
 
-function addNote(note) {
-	$("#notes").prepend(
-		$("<a href='#'></a>")
-		.addClass("note")
-		.text(note)
-	);
-}
+  function addNote(note) {
+  	$("#notes").prepend(
+  		$("<a href='#'></a>")
+  		.addClass("note")
+  		.text(note)
+  	);
+  }
 
-function addCurrentNote() {
-	var current_note = $("#note").val();
+  function addCurrentNote() {
+  	var current_note = $("#note").val();
 
-	if (current_note) {
-		notes.push(current_note);
-		addNote(current_note);
-		$("#note").val("");
-	}
-}
+  	if (current_note) {
+  		notes.push(current_note);
+  		addNote(current_note);
+  		$("#note").val("");
+  	}
+  }
 
-function showHelp() {
-	$("#help").show();
+  function showHelp() {
+  	$("#help").show();
 
-	document.addEventListener("click",function __handler__(evt){
-		evt.preventDefault();
-		evt.stopPropagation();
-		evt.stopImmediatePropagation();
+  	document.addEventListener("click",function __handler__(evt){
+  		evt.preventDefault();
+  		evt.stopPropagation();
+  		evt.stopImmediatePropagation();
 
-		document.removeEventListener("click",__handler__,true);
-		hideHelp();
-	},true);
-}
+  		document.removeEventListener("click",__handler__,true);
+  		hideHelp();
+  	},true);
+  }
 
-function hideHelp() {
-	$("#help").hide();
-}
+  function hideHelp() {
+  	$("#help").hide();
+  }
 
-function handleOpenHelp(evt) {
-	if (!$("#help").is(":visible")) {
-		evt.preventDefault();
-		evt.stopPropagation();
+  function handleOpenHelp(evt) {
+  	if (!$("#help").is(":visible")) {
+  		evt.preventDefault();
+  		evt.stopPropagation();
 
-		showHelp();
-	}
-}
+  		showHelp();
+  	}
+  }
 
-function handleAddNote(evt) {
-	addCurrentNote();
-}
+  function handleAddNote(evt) {
+  	addCurrentNote();
+  }
 
-function handleEnter(evt) {
-	if (evt.which == 13) {
-		addCurrentNote();
-	}
-}
+  function handleEnter(evt) {
+  	if (evt.which == 13) {
+  		addCurrentNote();
+  	}
+  }
 
-function handleDocumentClick(evt) {
-	$("#notes").removeClass("active");
-	$("#notes").children(".note").removeClass("highlighted");
-}
+  function handleDocumentClick(evt) {
+  	$("#notes").removeClass("active");
+  	$("#notes").children(".note").removeClass("highlighted");
+  }
 
-function handleNoteClick(evt) {
-	evt.preventDefault();
-	evt.stopPropagation();
+  function handleNoteClick(evt) {
+  	evt.preventDefault();
+  	evt.stopPropagation();
 
-	$("#notes").addClass("active");
-	$("#notes").children(".note").removeClass("highlighted");
-	$(evt.target).addClass("highlighted");
-}
+  	$("#notes").addClass("active");
+  	$("#notes").children(".note").removeClass("highlighted");
+  	$(evt.target).addClass("highlighted");
+  }
 
-function init() {
-	// build the initial list from the existing `notes` data
-	var html = "";
-	for (i=0; i<notes.length; i++) {
-		html += "<a href='#' class='note'>" + notes[i] + "</a>";
-	}
-	$("#notes").html(html);
+  function bulkAdd() {
+    console.log('hi');
+  }
 
-	// listen to "help" button
-	$("#open_help").bind("click",handleOpenHelp);
+  function init() {
+  	// build the initial list from the existing `notes` data
+  	var html = "";
+    for (i=0; i<notes.length; i++) {
+      html += "<a href='#' class='note'>" + notes[i] + "</a>";
+    }
+    $("#notes").html(html);
 
-	// listen to "add" button
-	$("#add_note").bind("click",handleAddNote);
+    // listen to "help" button
+    $("#open_help").bind("click",handleOpenHelp);
 
-	// listen for <enter> in text box
-	$("#new_note").bind("keypress",handleEnter);
+    // listen to "add" button
+    $("#add_note").bind("click",handleAddNote);
 
-	// listen for clicks outside the notes box
-	$(document).bind("click",handleDocumentClick);
+    // listen for <enter> in text box
+    $("#new_note").bind("keypress",handleEnter);
 
-	// listen for clicks on note elements
-	$("#notes").on("click",".note",handleNoteClick);
-}
+    // listen for clicks outside the notes box
+    $(document).bind("click",handleDocumentClick);
 
-$(document).ready(init);
+    // listen for clicks on note elements
+    $("#notes").on("click",".note",handleNoteClick);
+
+  }
+
+  var publicAPI = {
+    init: init,
+    bulkAdd: bulkAdd
+  };
+
+  return publicAPI;
+})();
+
+$(document).ready(NotesManager.bulkAdd)
+  .ready(NotesManager.init);
